9bf82286e870309ef1ce0941b580985b46ca97f2
adelon <22380201+adelon@users.noreply.github.com>
2020-05-26 00:25:15 +0200

    Plan Agda translation

--- source/Translation/ToAgda.hs
+++ source/Translation/ToAgda.hs
@@ -1,5 +1,28 @@
 {-# LANGUAGE FunctionalDependencies #-}
 
+
+-- Direct translation to Agda.
+
+
+-- TODO.
+-- Decide between `Concrete` and `Abstract` variants of Agda's syntax.
+--
+-- `Concrete` requires desugaring by Agda and re-does some of the work
+-- that we did when parsing to our AST (e.g.: operator prec).
+-- On the upside, it corresponds directly to Agda's surface syntax.
+--
+-- `Abstract` needs a bit of extra information for some nodes and probably
+-- also more work on our AST (e.g.: binding variables properly?). Could be
+-- the more morally correct choice.
+
+
+-- TODO
+-- It should be possible to decorate the translated AST with appropriate
+-- source ranged (from `Agda.Syntax.Position`) and get the correct ranges
+-- from Agda's type checker. We can then use those ranges to show the
+-- correct range in the controlled natural language input.
+
+
 module Translation.ToAgda where
 
 
@@ -34,6 +57,10 @@ class ToAgda a c | a -> c where
 instance ToAgda Expr AConc.Expr where
    toAgda = \case
       ExprVar (Var x) -> pure (AConc.Ident (mkQName x))
+
+      ExprParen e -> do
+         e' <- toAgda e
+         pure (AConc.Paren APos.NoRange e')
       --
       -- Using `RawApp` for now, since the actual `App` requires more metadata.
       e1 `ExprApp` e2 -> do
@@ -41,7 +68,19 @@ instance ToAgda Expr AConc.Expr where
          e2' <- toAgda e2
          pure (AConc.RawApp APos.NoRange [e1', e2'])
 
+      ExprChain ch -> toAgda ch
+
       _ -> error "toAgda incomplete"
+
+
+instance ToAgda Chain AConc.Expr where
+   toAgda = \case
+      ChainBase es -> do
+         es' <- forM es toAgda
+         pure (foldr1 conj es')
+         where
+            conj e1 e2 = error "conj in toAgda undefined"
+      ChainCons es rel ch -> error "toAgda incomplete"
 
 
 mkQName :: Text -> AConc.QName
